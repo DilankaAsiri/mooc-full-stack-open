5c3244a85dc49c37340c33880c693f624bf70f6b
Dilanka Asiri <DilankaAsiri@users.noreply.github.com>
2024-03-03 23:39:45 +0200

    Completed : 3.18

--- part3/phonebook/client/src/App.jsx
+++ part3/phonebook/client/src/App.jsx
@@ -54,13 +54,15 @@ const App = () => {
         `${person.name} is already added to phonebook, replace the old number with a new one?`
       )
     ) {
-      const { id } = existingPerson;
-      personService.update(id, { number: person.number }).then((personRes) => {
-        setPersonsData(
-          persons.map((p) => (p.id !== personRes.id ? p : personRes))
-        );
-        showNotification(`Updated ${personRes.name}`);
-      });
+      const { id, ...rest } = existingPerson;
+      personService
+        .update(id, { ...rest, number: person.number })
+        .then((personRes) => {
+          setPersonsData(
+            persons.map((p) => (p.id !== personRes.id ? p : personRes))
+          );
+          showNotification(`Updated ${personRes.name}`);
+        });
       return;
     }
   };
--- part3/phonebook/server/index.js
+++ part3/phonebook/server/index.js
@@ -8,12 +8,23 @@ const Person = require("./models/person");
 
 morgan.token("body", (req, res) => JSON.stringify(req.body));
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
 app.use(express.json());
 app.use(cors());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 app.use(express.static("dist"));
+app.use(errorHandler);
 
 app.get("/info", (request, response) => {
   response.send(
@@ -23,7 +34,7 @@ app.get("/info", (request, response) => {
   );
 });
 
-app.post("/api/persons", async (request, response) => {
+app.post("/api/persons", async (request, response, next) => {
   const body = request.body;
 
   if (!(body.name && body.number)) {
@@ -48,43 +59,54 @@ app.post("/api/persons", async (request, response) => {
     number: body.number,
   });
 
-  person = await person.save();
-  response.json(person);
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/api/persons/:id", async (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
   const id = request.params.id;
-  const person = await Person.findById(id);
-  response.json(person);
+  Person.findById(id)
+    .then((person) => {
+      response.json(person);
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/api/persons", async (request, response) => {
-  const persons = await Person.find({});
-  response.json(persons);
+app.get("/api/persons", (request, response, next) => {
+  Person.find({})
+    .then((persons) => {
+      response.json(persons);
+    })
+    .catch((error) => next(error));
 });
 
-app.patch("/api/persons/:id", async (request, response) => {
+app.put("/api/persons/:id", (request, response, next) => {
   const id = request.params.id;
   const body = request.body;
 
-  let person = await Person.findById(id);
-  if (!person) {
-    return response.status(404).json({
-      error: "person not found",
-    });
-  }
+  let person = {
+    name: body.name,
+    number: body.number,
+  };
 
-  if (body.number) person.number = body.number;
-  person.isNew = false;
-
-  person = await person.save();
-  response.json(person);
+  Person.findByIdAndUpdate(id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", async (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
   const id = request.params.id;
-  await Person.findByIdAndDelete(id);
-  response.status(204).end();
+  Person.findByIdAndDelete(id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 const PORT = process.env.PORT || 3001;
