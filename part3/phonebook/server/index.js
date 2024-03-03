2a0d157c9ce2503645a012569b1f3bfb2e20720a
Dilanka Asiri <DilankaAsiri@users.noreply.github.com>
2024-03-03 21:32:45 +0200

    Completed : Part 3, step 11

--- part3/phonebook/server/index.js
+++ part3/phonebook/server/index.js
@@ -10,6 +10,7 @@ app.use(cors())
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
+app.use(express.static('dist'))
 
 const generateId = () => {
   return Math.floor(Math.random() * 999999999999999);
@@ -91,7 +92,7 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
