aaf66ecd23ab007195dbd1146b1bef192831b273
Dilanka Asiri <DilankaAsiri@users.noreply.github.com>
2024-03-03 22:07:12 +0200

    Completed : 3.12

--- part3/phonebook/server/index.js
+++ part3/phonebook/server/index.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-const cors = require('cors')
+const cors = require("cors");
 
 morgan.token("body", (req, res) => JSON.stringify(req.body));
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 
 const generateId = () => {
   return Math.floor(Math.random() * 999999999999999);
@@ -92,7 +92,7 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
---
+++ part3/phonebook/server/mongo.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const mongoPassword = process.argv[2];
+const url = `mongodb+srv://ghostmongodb:${mongoPassword}@cluster0.otps0wq.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.set("strictQuery", false);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+const Person = mongoose.model("Person", personSchema);
+
+const name = process.argv[3];
+const number = process.argv[4];
+
+const main = async () => {
+  await mongoose.connect(url);
+
+  if (name && number) {
+    const person = new Person({
+      name,
+      number,
+    });
+    person.save().then(() => {
+      console.log(`Added ${name} number ${number} to phonebook`);
+      mongoose.connection.close();
+    });
+  } else {
+    Person.find({}).then((result) => {
+      console.log("phonebook:");
+      result.forEach((person) => {
+        console.log(`${person.name} ${person.number}`);
+      });
+      mongoose.connection.close();
+    });
+  }
+};
+
+main();
