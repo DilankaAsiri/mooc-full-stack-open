8b25cdaf74e8f44dca9ea67bd71995dbfa7eab12
Dilanka Asiri <DilankaAsiri@users.noreply.github.com>
2024-03-12 11:22:52 +0200

    Done : task 4.7

--- part4/blog/server/utils/list_helper.js
+++ part4/blog/server/utils/list_helper.js
@@ -30,9 +30,24 @@ const mostBlog = (blogs) => {
   return _.maxBy(mostBlogSummary, "blogs") ?? null;
 };
 
+const mostLikes = (blogs) => {
+  const mostLikesSummary = _(blogs)
+    .groupBy("author")
+    .map((blogsArr, author) => {
+      return {
+        author,
+        likes: blogsArr.reduce((acc, cur) => acc + cur.likes, 0),
+      };
+    })
+    .value();
+
+  return _.maxBy(mostLikesSummary, "likes") ?? null;
+};
+
 module.exports = {
   dummy,
   mostBlog,
+  mostLikes,
   totalLikes,
   favouriteBlog,
 };
